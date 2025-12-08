f136256e43fa81822ef828d1af62eedb4888202f
Oskar Wreiber <oskar.wreiber@ericsson.com>
2025-12-08 09:50:46 +0100

    fix: use CORS proxy for Courtyard, Playwright only for Food & Co

--- src/js/fetchers/courtyard.js
+++ src/js/fetchers/courtyard.js
@@ -5,7 +5,7 @@ import { ParseError } from '../utils/errors.js';
 
 export class CourtyardFetcher extends MenuFetcher {
     constructor() {
-        super('https://ericssonbynordrest.se/restaurang/the-courtyard/', true);
+        super('https://ericssonbynordrest.se/restaurang/the-courtyard/', false);
     }
 
     parse(html) {
